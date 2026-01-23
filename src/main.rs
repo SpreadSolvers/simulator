b35d3413918f8f8df75d1dae43891d481f2566eb
semidol <0xmarvin42@email.com>
2026-01-23 14:55:25 +0300

    fix(main): return err if token address is without bytecode

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ use alloy::{
 };
 use revm::{
     Context, ExecuteEvm, InspectEvm, Inspector, MainBuilder, MainContext,
-    context::{TxEnv, result::EVMError, tx::TxEnvBuildError},
+    context::{
+        TxEnv,
+        result::{EVMError, SuccessReason},
+        tx::TxEnvBuildError,
+    },
     context_interface::result::ExecutionResult,
     database::{AlloyDB, CacheDB, DBTransportError, EmptyDB, WrapDatabaseAsync},
     interpreter::{
@@ -90,17 +94,22 @@ async fn main() -> anyhow::Result<()> {
     let rpc_url: Url = "https://eth.drpc.org".parse()?;
     let rpc_url: Url = "https://ethereum-rpc.publicnode.com".parse()?;
     let rpc_url: Url = "https://rpc.flashbots.net".parse()?;
+    // let rpc_url: Url = "https://rrpc.flashbots.net".parse()?;
 
     let zro_address = address!("0x6985884C4392D348587B19cb9eAAf157F13271cd");
     let zro_holder_address = address!("0x1d4A12A09C293b816BFF3625abdA3Ae07dee19F5");
     let usdc_address = address!("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
     let usdc_holder = address!("0xB166b43B24c2e42A12b2F788Ae0EFA536A914530");
+    let empty_address = address!("0x183722431Db2CFb8145C939ab3C6d759bE8CeDDe");
 
-    let zro_slot = find_balance_slot(zro_address, zro_holder_address, rpc_url.clone())?;
-    println!("zro {zro_slot:?}");
+    let slot = find_balance_slot(empty_address, empty_address, rpc_url.clone())?;
+    println!("slot {slot:?}");
 
-    let usdc_slot = find_balance_slot(usdc_address, usdc_holder, rpc_url.clone())?;
-    println!("usdc {usdc_slot:?}");
+    // let zro_slot = find_balance_slot(zro_address, zro_holder_address, rpc_url.clone())?;
+    // println!("zro {zro_slot:?}");
+    //
+    // let usdc_slot = find_balance_slot(usdc_address, usdc_holder, rpc_url.clone())?;
+    // println!("usdc {usdc_slot:?}");
 
     Ok(())
 }
@@ -159,6 +168,8 @@ enum FindSlotError {
 enum InspectBalanceOfError {
     TxBuild(TxEnvBuildError),
     InspectError(#[from] EVMError<DBTransportError>),
+    #[error("execution failed: {0:?}")]
+    Execution(ExecutionResult),
 }
 
 impl From<TxEnvBuildError> for InspectBalanceOfError {
@@ -180,9 +191,15 @@ fn inspect_balance_of(
 
     let tx = build_balance_of_tx_env(token_address, user_address)?;
 
-    evm.inspect_one_tx(tx)?;
+    let res = evm.inspect_one_tx(tx)?;
 
-    Ok(evm.inspector)
+    match res {
+        ExecutionResult::Success {
+            reason: SuccessReason::Return,
+            ..
+        } => Ok(evm.inspector),
+        failed => Err(InspectBalanceOfError::Execution(failed)),
+    }
 }
 
 fn build_balance_of_tx_env(
